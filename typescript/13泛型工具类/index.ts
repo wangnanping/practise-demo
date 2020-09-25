0b94d9aef7b44b5ed73b8430c2cc02408f1f18f4
wang <1341540464@qq.com>
2020-09-25 17:48:28 +0800

    addad:泛型工具类型 Partial、Required、Readonly......

--- typescript/13泛型工具类/index.ts
+++ typescript/13泛型工具类/index.ts
@@ -1,1 +1,34 @@
 // 泛型工具类型
+
+interface Person {
+  name: string;
+  age?: number;
+}
+
+// Partial
+
+// 源码
+// type Partial<T> = {
+//   [P in keyof T]?: [P];
+// };
+
+type person = Partial<Person>;
+// person === {name?: string; age?: number}
+
+// Required
+
+// type Required<T> = {
+//     [P in keyof T]-?: T[P];
+// };
+
+type persons = Required<Person>;
+// persons === {name: string; age: number}
+
+// Readonly
+
+// type Readonly<T> = {
+//     readonly [P in keyof T]: T[P];
+// };
+
+type personss = Required<Person>;
+// personss === {readonly name: string; readonly age?: number}
