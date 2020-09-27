5df435cc19383f13593bbf74863eb2d2c5fe743d
wang <1341540464@qq.com>
2020-09-27 18:36:25 +0800

    装饰器

--- typescript/14装饰器/index.ts
+++ typescript/14装饰器/index.ts
@@ -3,3 +3,15 @@
 // 例子 用类装饰器重现
 // https://www.jianshu.com/p/022988da4dc1?utm_campaign=maleskine&utm_content=note&utm_medium=seo_notes&utm_source=recommendation
 
+function Greeter(target: any) {
+  target.proptype.getText = function () {
+    console.log("hello world");
+  };
+}
+
+@Greeter()
+class MyGerrter() {
+    consturctor(){
+        
+    }
+}
